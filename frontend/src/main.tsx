e53c231f468a5e9f85dd052f5b125a30193ad643
Jury Soldatenkov <yurysold@gmail.com>
2026-06-16 15:57:09 +0000

    Fixed calendar at owners page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@mantine/core/styles.css'
+import '@mantine/dates/styles.css'
+import '@mantine/schedule/styles.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
